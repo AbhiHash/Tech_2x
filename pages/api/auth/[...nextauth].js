a831ccb4644ba1885da6b746127c7bbfb6cce44c
Abhishek Tripathi <abhitripathi9800q@gmail.com>
2023-09-16 12:53:38 +0530

    Added Db Connection File

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import { ensureDbConnected } from "../../../lib/dbConnect";
+import { User } from "../../../lib/db";
 export const authOptions = {
   providers: [
     CredentialsProvider({
@@ -12,17 +13,45 @@ export const authOptions = {
       },
 
       async authorize(credentials, req) {
-        const user = {
-          name: credentials.username,
-        };
+        await ensureDbConnected();
+        if (!credentials) {
+          return null;
+        }
+        const username = credentials.username;
+        const password = credentials.password;
+        const user = await User.findOne({ username });
 
-        if (user) {
-          return Promise.resolve(user);
+        if (!user) {
+          const obj = { username: username, password: password };
+          const newUser = new User(obj);
+          let userDb = await newUser.save();
+          console.log(userDb);
+          return {
+            id: userDb._id,
+            email: userDb.username,
+          };
         } else {
-          // Authentication failed
-          throw new Error("Invalid credentials");
+          if (user.password !== password) {
+            return null;
+          }
+          return {
+            id: user._id,
+            email: user.username,
+          };
         }
       },
+
+      // const user = {
+      //   name: credentials.username,
+      // };
+
+      // if (user) {
+      //   return Promise.resolve(user);
+      // } else {
+      //   // Authentication failed
+      //   throw new Error("Invalid credentials");
+      // }
+      // },
     }),
   ],
 };
