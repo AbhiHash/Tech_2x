9b0ebe382aabe5e9ef21d016643d932b3a431a92
harrsshitaSingh <harshita.singh@tessact.com>
2023-09-20 12:18:50 +0530

    Course css done

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -50,25 +50,3 @@ export const CustomNavbar = (props: NavbarProps) => {
     </div>
   );
 };
-
-{
-  /* <div className=" inline-flex w-1/12 justify-between text-slate-200/100 ">
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
-            />
-          </svg>
-          LOGOUT
-        </button>
-      </div> */
-}
