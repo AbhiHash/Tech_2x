5f1611abb7f25f16adc6f90a45367d498c09367d
AbhiHash <hsingharshita01@gmail.com>
2023-10-20 14:51:29 +0530

    Added

--- components/main.tsx
+++ components/main.tsx
@@ -9,7 +9,7 @@ export const MainContent = () => {
         <h1 className="text-slate-200 flex justify-start text-2xl font-bold ml-10 pt-10">
           COURSES
         </h1>
-        <div className="flex flex-col md:flex-row justify-evenly align-middle">
+        <div className="flex flex-col md:flex-row justify-evenly align-middle cursor-pointer">
           <CustomCard></CustomCard>
           <CustomCard></CustomCard>
           <CustomCard></CustomCard>
